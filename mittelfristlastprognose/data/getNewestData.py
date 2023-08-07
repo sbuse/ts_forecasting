e24d4a7bfd6e5c54ecb749efdfeb315f5ef67298
sbuse <simon.buse@hotmail.com>
2023-08-07 11:54:43 +0200

    improving monthly predictions

--- mittelfristlastprognose/data/getNewestData.py
+++ mittelfristlastprognose/data/getNewestData.py
@@ -4,22 +4,19 @@ import sys
 import warnings
 warnings.filterwarnings("ignore")
 
-#read in flags given to this file at execution
-frequency = sys.argv[1] #expects a string like "d","m" for daily or monthly
-
-if frequency.lower() == "d":
-    folder = "daily"
-elif frequency.lower() == "m":
-    folder = "monthly"
-else:
-    raise ValueError("The frequency given is not valid. Please use 'd' for daily or 'm' for monthly.")
-  
 def resample_fix_ends(pdf,frequency):
   """
   The function resamples the data according to the sampling frequency. 
   Often the first and the last data-point are deviating a lot from the rest of the series.
   As a simple fix i will just delete the first and the last value if they deviate more than 20% to their neighbour. 
   """
+  #if input pdf is not a pandas dataframe, try to convert it to one
+  if not isinstance(pdf, pd.DataFrame):
+    try: 
+      pdf = pd.DataFrame(pdf)
+    except: 
+      raise ValueError("The input pdf can not be converted to a pandas dataframe.")
+    
   pdf = pdf.resample(frequency.upper()).sum(min_count=1) #"D,W,M"
   for column in pdf.columns:
     if pdf[column].iloc[0] < 0.8*pdf[column].iloc[1]:
@@ -40,23 +37,36 @@ def reassign_outliers(pdf):
   return pdf
 
 
-url = "https://data.stadt-zuerich.ch/dataset/ewz_stromabgabe_netzebenen_stadt_zuerich/download/ewz_stromabgabe_netzebenen_stadt_zuerich.csv"
-pdf = pd.read_csv(url,index_col=None)
+if __name__ == "__main__":
+   
+  #read in flags given to this file at execution
+  frequency = sys.argv[1] #expects a string like "d","m" for daily or monthly
+
+  if frequency.lower() == "d":
+      folder = "daily"
+  elif frequency.lower() == "m":
+      folder = "monthly"
+  else:
+      raise ValueError("The frequency given is not valid. Please use 'd' for daily or 'm' for monthly.")
 
 
-pdf["Timestamp"] =  pd.to_datetime(pdf['Timestamp'],utc=True)
-pdf = pdf.set_index(pdf["Timestamp"])
-pdf = resample_fix_ends(pdf,frequency)
-pdf = reassign_outliers(pdf)
-
-pdf.index = pdf.index.tz_localize(None)  #Let's drop the timezone info to avoid warnings
+  url = "https://data.stadt-zuerich.ch/dataset/ewz_stromabgabe_netzebenen_stadt_zuerich/download/ewz_stromabgabe_netzebenen_stadt_zuerich.csv"
+  pdf = pd.read_csv(url,index_col=None)
 
 
-pdf["NE5_GWh"] = pdf["Value_NE5"].values/1e6 #in GWh
-pdf["NE7_GWh"] = pdf["Value_NE7"].values/1e6 #in GWh
+  pdf["Timestamp"] =  pd.to_datetime(pdf['Timestamp'],utc=True)
+  pdf = pdf.set_index(pdf["Timestamp"])
+  pdf = resample_fix_ends(pdf,frequency)
+  pdf = reassign_outliers(pdf)
 
-pdf = pdf.drop(columns=["Value_NE5","Value_NE7"])
+  pdf.index = pdf.index.tz_localize(None)  #Let's drop the timezone info to avoid warnings
 
-pdf.to_csv(f"{folder}/ewz_stromabgabe_netzebenen_stadt_zuerich.csv")
 
-print("downloaded and saved the newest data.The last data-point is from: ",pdf.index[-1].strftime("%Y-%m-%d %H:%M:%S"))
+  pdf["NE5_GWh"] = pdf["Value_NE5"].values/1e6 #in GWh
+  pdf["NE7_GWh"] = pdf["Value_NE7"].values/1e6 #in GWh
+
+  pdf = pdf.drop(columns=["Value_NE5","Value_NE7"])
+
+  pdf.to_csv(f"{folder}/ewz_stromabgabe_netzebenen_stadt_zuerich.csv")
+
+  print("downloaded and saved the newest data.The last data-point is from: ",pdf.index[-1].strftime("%Y-%m-%d %H:%M:%S"))
